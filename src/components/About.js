cf487b54227c3c6e037008f37a9a4dba7d124d25
Antwoine Griggs <97367683+antwoinegriggs@users.noreply.github.com>
2023-05-23 19:39:10 -0400

    complete lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,19 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
-  return (
-    <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
-      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
-    </div>
-  );
+function About(user) {
+  if (!user.bio) {
+    return null;
+  } else {
+    return (
+      <div id="about">
+        <h2>About Me</h2>
+        <p>{user.bio}</p>
+        <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+        <Links github={user.github} linkedin={user.linkedin} />
+      </div>
+    );
+  }
 }
 
 export default About;
